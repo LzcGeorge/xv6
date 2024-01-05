39afa2df04ff3fdb52adb486e4f815d190d125b6
LzcGeorge <l2019math@163.com>
2024-01-05 12:38:37 +0800

    my solution for util lab exercise 2:pingpong

--- user/pingpong.c
+++ user/pingpong.c
@@ -5,13 +5,37 @@
 int 
 main(int argc, char *argv[])
 {
-    int n;
-    if(argc < 2){
-        fprintf(2, "Usage: argc error message...\n");
-        exit(1);
-    }
+    int p_child[2];
+    int p_parent[2];
+    char buffer[8]; // 一个字节
+    pipe(p_child);
+    pipe(p_parent);
+    // p[0] is read,p[1] is write
     
-    n = atoi(argv[1]);
-    sleep(n);
-    exit(0);
+    int pid = fork();
+
+    if(pid == 0) { // child
+   
+        // close(p[1]);
+        read(p_child[0],buffer,8);
+        printf("%d: received ping\n",getpid());
+        close(p_child[0]);
+
+        // write the byte to the parents
+        write(p_parent[1], "child", sizeof("child"));
+        close(p_parent[1]);
+        exit(0);
+    } else { // parents
+        // write the byte to the child
+        write(p_child[1], "parent", sizeof("parent"));
+        close(p_child[1]);
+
+        read(p_parent[0],buffer,8);
+        wait(0);
+        printf("%d: received pong\n",getpid());
+        
+        close(p_parent[0]);
+        exit(0);
+    }
+   
 }
